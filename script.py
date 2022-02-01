7eaca77aa70d34e6495cb485b48fd8f1e9e377af
deimos <lozadacja@gmail.com>
2022-02-01 00:52:18 -0300

    We add a validation while whourse to the GENRE step

--- script.py
+++ script.py
@@ -37,19 +37,25 @@ pause()
 print('Now choose your genre: (1, 2 or 3)')
 print('1) Male')
 print('2) Female')
-print("3) I'm an unemployed indecisive hippie")
+print("3) I can be both ;)")
 
 user_genre = input().lower()
 
-if(user_genre == '1') or (user_genre == 'male'):
+
+i = 0
+while (user_genre != '1') and (user_genre != 'male') and (user_genre != 'm') and (user_genre != '2') and (user_genre != 'female') and (user_genre != 'f') and (user_genre != '3') and (user_genre != 'hippie') and (user_genre != 'n') and (i<3):
+    print(f'Sorry, {user_name}, the selected option is invalid, please try again (1, 2 or 3):')
+    user_genre = input().lower()
+    i=i+1
+
+if(user_genre == '1') or (user_genre == 'male') or (user_genre == 'm'):
     print(f'The user genre is Male')
 
-elif(user_genre == '2') or (user_genre == 'female'):
+elif(user_genre == '2') or (user_genre == 'female') or (user_genre == 'f'):
     print(f'The user gente is Female')
 
-elif(user_genre == '3') or (user_genre == 'hippie'):
+elif(user_genre == '3') or (user_genre == 'hippie') or (user_genre == 'n'):
     print(f'The user genre is Neutral')
 
 else:
-    print(f'wrong')
-    sys.exit()
+    print(f"Alright, {user_name}, I can see you can't read. Come back when you learn.")
